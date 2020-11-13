d8018853b07b8d0c0f80072e7f4beb65ddc6dacf
rehetkapoor <70991670+rehetkapoor@users.noreply.github.com>
2020-11-12 18:50:56 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,107 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var Stand, sling, circle
+var BG=0
+
+var block1, block2, block3, block4, block5
+var block6, block7, block8
+var block9
+
+var score =0
+
+function preload() {
+
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    back();
+    engine = Engine.create();
+    world = engine.world;
+
+    Stand = new Ground(590, 240, 350,20);
+    block1 = new Box(530,235,30,40);
+    block2 = new Box(560, 235, 30, 40);
+    block3 = new Box(590,235,30,40);
+    block4 = new Box(620,235,30,40);
+    block5 = new Box(650, 235, 30, 40);
+
+    block6 = new Box(560, 195, 30, 40);
+    block7 = new Box(590, 195, 30, 40);
+    block8 = new Box(620, 195, 30, 40);
+
+    block9 = new Box(590, 155, 30, 40);
+
+    circle = new Circle(115,180);
+
+    slingshot = new Slingshot(circle.body,{x:115, y:180});
+
+
+}
+
+function draw(){
+    background("black")
+    if (BG===1){
+    background("grey");
+    }
+    Engine.update(engine);
+    Stand.display();
+    block1.display();
+    block2.display();
+    block3.display();
+    block4.display();
+    block5.display();
+    block6.display();
+    block7.display();
+    block8.display();
+    block9.display();
+    circle.display();
+    slingshot.display();
+    text("Score: " + score, 750, 40);
+    block1.score();
+    block2.score();
+    block3.score();
+    block4.score();
+    block5.score();
+    block6.score();
+    block7.score();
+    block8.score();
+    block9.score();
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(circle.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(circle.body);
+    }
+}
+
+async function back (){
+    var data = await fetch("http://worldtimeapi.org/api/timezone/America/Los_Angeles");
+
+    var J = await data.json();
+
+    var DT = J.datetime
+
+    var H = DT.slice(11,13)
+
+    console.log(H);
+
+    if (H>=06 && H<=18){
+        BG=1
+    } else{
+        BG=0
+    }
+}
